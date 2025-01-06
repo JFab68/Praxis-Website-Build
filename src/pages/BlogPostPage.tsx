c2b50a3b7022c28eb95ed8863e61f86d4992a0dd
JFab68 <42310697+JFab68@users.noreply.github.com>
2025-01-06 00:50:04 -0700

    Add files via upload

---
+++ src/pages/BlogPostPage.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
+import BlogPost from '../components/blog/BlogPost';
+import { blogPosts } from '../data/blog-posts';
+
+const BlogPostPage = () => {
+  const { slug } = useParams<{ slug: string }>();
+  const post = blogPosts.find(post => post.slug === slug);
+
+  if (!post) {
+    return <Navigate to="/blog" replace />;
+  }
+
+  return (
+    <div className="pt-16">
+      <div className="py-20">
+        <div className="container mx-auto px-4">
+          <BlogPost post={post} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogPostPage;
